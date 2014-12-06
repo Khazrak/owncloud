e141e6d7ff29ffa994f92c6d29a684a7c23802a5
Karl Andersson <karl.andersson1987@gmail.com>
2014-12-06 18:36:10 +0100

    More fixes

--- config.sh
+++ config.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-
+echo "ServerName $SERVER_NAME:443" | sudo tee /etc/apache2/conf-available/fqdn.conf;
+a2enconf fqdn;
 sed -i "s/SSLKEY/$SSL_KEY/g" /etc/apache2/sites-available/default-ssl.conf;
 sed -i "s/S_NAME/$SERVER_NAME/g" /etc/apache2/sites-available/default-ssl.conf;
 sed -i "s/S_NAME/$SERVER_NAME/g" /etc/apache2/sites-available/default-ssl.conf;
 sed -i "s/SSLCERT/$SSL_CERT/g" /etc/apache2/sites-available/default-ssl.conf;
-echo "ServerName $SERVER_NAME:443" | sudo tee /etc/apache2/conf-available/fqdn.conf
-a2enconf fqdn
-
-/usr/sbin/apache2ctl start
+/usr/sbin/apache2ctl -D FOREGROUND
